e8ebd68a030ac3281e075914db41c372661ba2e4
iQrus <39492397+iQrus@users.noreply.github.com>
2020-05-10 03:23:33 +0530

    added config file, mock json and api-endpoint generator utility

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configs {
-    public readonly API_ENDPOINT: string = 'https://domain.com/api';
-    public readonly API_MOCK_ENDPOINT: string = 'https://next.json-generator.com/api/json/get';
+    public readonly API_ENDPOINT: string = 'https://q42oh26fu8.execute-api.ap-south-1.amazonaws.com/default/';
+    public readonly API_MOCK_ENDPOINT: string = 'assets/mock-data/search-result-response.json';
+
+    public readonly ENABLE_MOCK_RESPONE: boolean = false;
+    
 }
---
+++ src/app/constants/constants.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Constants {
+    public readonly API_ACTION: string = 'sentimentAnalysis';
+    public readonly PARAM_KEYWORD: string = 'keyword'
+    
+}
