a32ac8102099ee0296c886d1917d82e478578f65
iQrus <39492397+iQrus@users.noreply.github.com>
2020-05-13 01:29:06 +0530

    again forgot to remove api key :P will put it in ignore list

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
 export class Configs {
     public readonly API_ENDPOINT: string = 'https://21ngnglbsj.execute-api.ap-south-1.amazonaws.com/test';
     public readonly API_MOCK_ENDPOINT: string = 'assets/mock-data/search-result-response.json';
-    public readonly X_API_KEY: string = 'wAx9ccOSAKa02lrE3zSPnyMV7tUTyFzEJUcmu980';
-
+    
     
     public readonly ENABLE_MOCK_RESPONE: boolean = true;
     
