30c33c860afa7b056a74c97a6444a018efe1be4d
iQrus <39492397+iQrus@users.noreply.github.com>
2020-05-27 02:00:54 +0530

    removing config details

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
 export class Configs {
     public readonly API_ENDPOINT: string = 'https://21ngnglbsj.execute-api.ap-south-1.amazonaws.com/test';
     public readonly API_MOCK_ENDPOINT: string = 'assets/mock-data/search-result-response1.json';
-    public readonly X_API_KEY: string = 'wAx9ccOSAKa02lrE3zSPnyMV7tUTyFzEJUcmu980';
+    
 
     
-    public readonly ENABLE_MOCK_RESPONE: boolean = true;
+    public readonly ENABLE_MOCK_RESPONE: boolean = false;
     
 }
